aeaed731ae179f3436dc4408c74a80ef61cf6d7a
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-10-06 05:48:37 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Settlers
+  </title>
+  <link href="./style.css" rel="stylesheet">
+</head>
+
+<body>
+  <h1 class="home__heading">Only the dead have seen the end of war -Plato </h1>
+
+  <form action="login.php" method="post">
+    <h2>Welcome Back</h2>
+    <input type="email" name="email" placeholder="Email"><br>
+    <input type="password" name="password" placeholder="Password"><br>
+    <input type="submit" value="Log In">
+  </form>
+
+</body>
+
+</html>
