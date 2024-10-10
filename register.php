3b5f237fcbb8665cd65b92ad58b9e6714fb9e904
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-10-10 07:47:51 +0100

    feat: validate nick field

--- register.php
+++ register.php
@@ -1,5 +1,22 @@
 <?php
 session_start();
+if (isset($_POST['email'])) {
+  $validation_ok = true;
+
+  $nick = $_POST['nick'];
+
+  if ((strlen($nick) < 3) || (strlen($nick) > 20)) {
+    $validation_ok = false;
+    $_SESSION['e_nick'] = "Nick must be betwen 3 - 20 signs";
+  }
+
+  if ($validation_ok == true) {
+    //add user
+    echo 'udana walidacja';
+    exit();
+  }
+
+}
 
 ?>
 
@@ -10,17 +27,33 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Register free today!</title>
+  <style>
+    .error {
+      color: red;
+      margin-top: 10px;
+      margin-bottom: 10px;
+    }
+  </style>
 </head>
 
 <body>
   <form>
     Nickname: <br><input type="text" name="nick"><br>
+    <?php
+    if (isset($_SESSION['e_nick'])) {
+      echo '<div class="error">' . $_SESSION['e_nick'] . '</div>';
+      unset($_SESSION['e_nick']);
+    }
+
+    ?>
     E-mail: <br><input type="text" name="email"><br>
     Password: <br><input type="password" name="password1"><br>
     Repeat Password: <br><input type="password" name="password2"><br>
     <label>
       <input type="checkbox" name="terms">Accept terms and conditions<br>
     </label>
+
+    <input type="submit" value="Submit">
   </form>
 
 </body>
